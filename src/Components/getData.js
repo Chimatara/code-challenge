7e87d31e36d0e3c9c483446447a1306c78a83ade
Chimatara Kalu <taradaniel05@gmail.com>
2024-06-30 21:43:11 +0100

    data fetched

--- src/Components/getData.js
+++ src/Components/getData.js
@@ -1,6 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const getData = () => {
+  const { data } = await axios.get('https://raw.githubusercontent.com/EBEN4REAL/cavista_coding_challenge/main/feed/sample.json')
+   return data;
+};
 
-    return;
+const useFetchData = () => {
+    return useQuery();
 }
